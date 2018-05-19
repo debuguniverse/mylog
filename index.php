2b7cc7ca5f8bf2c3d7addbff7cb4059066ecbd00
debuguniverse <2516155956@qq.com>
2018-05-19 09:32:55 +0800

    init

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Mloong
+ * Date: 2018/5/19
+ *
+ */
+
+class Mylog
+{
+    public function init(){
+
+    }
+
+    public function getLog(){
+
+    }
+
+    public function log(){
+
+    }
+}
---
+++ src/Filelog.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Mloong
+ * Date: 2018/5/19
+ *
+ */
+
+namespace debuguniverse;
+
+class Filelog
+{
+
+}
